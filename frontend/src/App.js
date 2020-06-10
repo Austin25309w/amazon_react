ee20cb0167aff8c320f0b2641dd4be72374c7bee
Austin25309w <austinow@gmail.com>
2020-06-10 12:09:23 -0700

    created server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import data from './data';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
 
 function App() {
   const openMenu = () => {
@@ -12,13 +13,14 @@ function App() {
     document.querySelector(".sidebar").classList.remove("open");
   }
   return (
+    <BrowserRouter>
     <div className="grid-container">
             <header className="header">
                 <div className="brand">
                     <button onClick={openMenu}>
                         &#9776;
                     </button>
-                    <a href="index.html">amazon-react</a>
+                    <Link to="/">amazon-react</Link>
                 </div>
                 <div className="header-links">
                     <a href="cart.html">Cart</a>
@@ -41,28 +43,16 @@ function App() {
 
               <main className="main">
                   <div className="content">
-                      <ul className="products">
-                        {
-                          data.products.map(product => 
-                          <li>
-                            <div className="product">
-                              <img className="product-image" src ={product.image} alt ="product"/> 
-                                  <div className="product-name">
-                                      <a href="product.html">{product.name}</a>
-                                  </div>
-                                  <div className="product-brand">{product.brand}</div>
-                                  <div className="product-price">{product.price}</div>
-                          <div className="product-rating">{product.rating} Stars ({product.numReview })</div>
-                            </div>
-                          </li>)
-                        }
-                        </ul>
+                    <Route path="/product/:id" component={ProductScreen}/>
+                    <Route path="/" exact ={true} component={HomeScreen}/>
+                      
                     </div>
                 </main>
             <footer className="footer">
                 All rights reseverd.
             </footer>
       </div>
+      </BrowserRouter>
   );
 }
 
