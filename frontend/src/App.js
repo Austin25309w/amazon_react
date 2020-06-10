d9bb9448d640bab24ca78d3db34e4be18df15fc9
Austin25309w <austinow@gmail.com>
2020-06-09 23:31:36 -0700

    transferred code to React env

---
+++ frontend/src/App.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const openMenu = () => {
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () => {
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+  return (
+    <div className="grid-container">
+            <header className="header">
+                <div className="brand">
+                    <button onClick={openMenu}>
+                        &#9776;
+                    </button>
+                    <a href="index.html">amazon-react</a>
+                </div>
+                <div className="header-links">
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sign In</a>
+                </div>
+            </header>
+            <aside className="sidebar"> 
+                <h3>Shopping Categories</h3>
+                <button className="sidebar-close-button" onClick={closeMenu}>X</button>
+                <ul>
+                    <li>
+                        <a herf="Charger.htmls">Charger</a>
+
+                    </li>
+                    <li>
+                        <a herf="Powerbank.html">Powerbank</a>
+                    </li>
+                </ul>
+            </aside>
+
+              <main className="main">
+                  <div className="content">
+                      <ul className="products">
+                          <li>
+                              <div className="product">
+                                <img className="product-image" src ="images/a1.jpg" alt ="product"/> 
+                                    <div className="product-name">
+                                        <a href="product.html">Powercore+ 45w PD</a>
+                                    </div>
+                                    <div className="product-brand">Anker</div>
+                                    <div className="product-price">$129.99</div>
+                                    <div className="product-rating">4.5 stars (10 Reviews)</div>
+                              </div>
+                          </li>
+                          <li>
+                              <div className="product">
+                                <img className="product-image" src ="images/a1.jpg" alt ="product"/> 
+                                    <div className="product-name">
+                                        <a href="product.html">Powercore+ 45w PD</a>
+                                    </div>
+                                    <div className="product-brand">Anker</div>
+                                    <div className="product-price">$129.99</div>
+                                    <div className="product-rating">4.5 stars (10 Reviews)</div>
+                              </div>
+                          </li>
+                          <li>
+                              <div className="product">
+                                <img className="product-image" src ="images/a1.jpg" alt ="product"/> 
+                                    <div className="product-name">
+                                        <a href="product.html">Powercore+ 45w PD</a>
+                                    </div>
+                                    <div className="product-brand">Anker</div>
+                                    <div className="product-price">$129.99</div>
+                                    <div className="product-rating">4.5 stars (10 Reviews)</div>
+                              </div>
+                          </li>
+                          <li>
+                              <div className="product">
+                                <img className="product-image" src ="images/a1.jpg" alt ="product"/> 
+                                    <div className="product-name">
+                                        <a href="product.html">Powercore+ 45w PD</a>
+                                    </div>
+                                    <div className="product-brand">Anker</div>
+                                    <div className="product-price">$129.99</div>
+                                    <div className="product-rating">4.5 stars (10 Reviews)</div>
+                              </div>
+                          </li>
+                          <li>
+                              <div className="product">
+                                <img className="product-image" src ="images/a1.jpg" alt ="product"/> 
+                                    <div className="product-name">
+                                        <a href="product.html">Powercore+ 45w PD</a>
+                                    </div>
+                                    <div className="product-brand">Anker</div>
+                                    <div className="product-price">$129.99</div>
+                                    <div className="product-rating">4.5 stars (10 Reviews)</div>
+                              </div>
+                          </li>
+                          <li>
+                              <div className="product">
+                                <img className="product-image" src ="images/a1.jpg" alt ="product"/> 
+                                    <div className="product-name">
+                                        <a href="product.html">Powercore+ 45w PD</a>
+                                    </div>
+                                    <div className="product-brand">Anker</div>
+                                    <div className="product-price">$129.99</div>
+                                    <div className="product-rating">4.5 stars (10 Reviews)</div>
+                              </div>
+                          </li>
+                        </ul>
+                    </div>
+                </main>
+            <footer className="footer">
+                All rights reseverd.
+            </footer>
+      </div>
+  );
+}
+
+export default App;
